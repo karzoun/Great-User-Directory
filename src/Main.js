58f33b509eaa1d59bce32cd5d88ca517d67db65f
“karzoun” <karzoun_27@hotmail.com>
2020-11-09 18:37:55 -0800

    style

--- src/Main.js
+++ src/Main.js
@@ -6,26 +6,8 @@ export class Main extends Component {
     super(props);
     this.state = {
       employeeData: [],
+      direction: "reverse",
     };
-  }
-
-  render() {
-    return (
-      <div className="main-container">
-        <table className="main-table">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>DOB</th>
-            </tr>
-          </thead>
-          {this.renderEachEmployee()}
-        </table>
-      </div>
-    );
   }
 
   //on initial load of the component
@@ -35,44 +17,113 @@ export class Main extends Component {
       this.setState({
         employeeData: employeeData.data.results,
       });
+      console.log(employeeData);
+    });
+
+    this.handleReorder = this.handleReorder.bind(this);
+  }
+
+  getData() {
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  }
+
+  handleReorder() {
+    const employeeList = this.state.employeeData;
+    if (this.state.direction === "reverse") {
+      this.setState({
+        direction: "order",
+        employeeData: employeeList.sort((a, b) =>
+          a.name.first > b.name.first ? 1 : -1
+        ),
+      });
+    } else {
+      this.setState({
+        direction: "reverse",
+        employeeData: employeeList.sort((a, b) =>
+          a.name.first > b.name.first ? -1 : 1
+        ),
+      });
+    }
+  }
+
+  handleChange(event) {
+    console.log(this);
+    const employeeList = this.state.employeeData;
+    const searchVal = event.target.value;
+    const filteredSearch = employeeList.filter(
+      (employee) => employee.name.first.indexOf(searchVal) > -1
+    );
+    this.setState({
+      employeeData: filteredSearch,
     });
   }
 
-  getData = () => {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  };
-
-  renderEachEmployee = () => {
-    console.log("******", this.state.employeeData);
-    //check if there is data in array
-    if (this.state.employeeData.length > 0) {
-      this.state.employeeData.map(
-        ({ picture, name, phone, email, dob, id }) => {
-          return (
-            <tr key={id.value}>
-              <td>
-                <img src={picture.thumbnail} alt="user iamge" />
-              </td>
-              <td>
-                <h3>
-                  `${name.title} ${name.first} ${name.last}`
-                </h3>
-              </td>
-              <td>
-                <h3>{phone}</h3>
-              </td>
-              <td>
-                <h3>{email}</h3>
-              </td>
-              <td>
-                <h3>{dob.date}</h3>
-              </td>
+  render() {
+    return (
+      <div className="main-container">
+        <div>
+          <span>Search here</span>
+          <input className="search input" onChange={this.handleChange} />
+        </div>
+        <table className="main-table">
+          <thead>
+            <tr>
+              <th
+                className={"col-1"}
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  display: "flex",
+                  maxWidth: "unset",
+                }}
+              >
+                Image
+              </th>
+              <th className={"col-2"} onClick={this.handleReorder}>
+                Name
+              </th>
+              <th className={"col-3"}>Phone</th>
+              <th className={"col-3"}>Email</th>
+              <th className={"col-3"}>DOB</th>
             </tr>
-          );
-        }
-      );
-    }
-  };
+          </thead>
+          {this.state.employeeData.map(
+            ({ picture, name, phone, email, dob, id }) => {
+              return (
+                <tr key={id.value}>
+                  <td
+                    className={"col-1"}
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      display: "flex",
+                      maxWidth: "unset",
+                    }}
+                  >
+                    <img src={picture.thumbnail} alt="user iamge" />
+                  </td>
+                  <td className={"col-2"}>
+                    <h3>
+                      {name.title} {name.first} {name.last}
+                    </h3>
+                  </td>
+                  <td className={"col-3"}>
+                    <h3>{phone}</h3>
+                  </td>
+                  <td className={"col-3"}>
+                    <h3>{email}</h3>
+                  </td>
+                  <td className={"col-3"}>
+                    <h3>{dob.date}</h3>
+                  </td>
+                </tr>
+              );
+            }
+          )}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Main;
